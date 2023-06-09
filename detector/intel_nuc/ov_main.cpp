983a9e05ffef64bb798dad54297e3155615dac30
hanhaoxuan <hanhaoxuan@nebula-link.com>
2023-06-09 10:27:54 +0800

    s

--- detector/intel_nuc/ov_main.cpp
+++ detector/intel_nuc/ov_main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 #include "ov_detect.h"
 #include "detector.h"
 #include "common_structs.h"
 
 using namespace std;
+using namespace cv;
 
 int main()
 {
@@ -12,5 +14,27 @@ int main()
     load_config(config, json_path);
     OvO_Detector ov_detector;
     ov_detector.Init(config);
+
+    VideoCapture cap("test.mp4");
+    if (!cap.isOpened())
+    {
+        cout << "Error opening video file" << endl;
+        return -1;
+    }
+    double fps = cap.get(CAP_PROP_FPS);
+    int total_frames = cap.get(CAP_PROP_FRAME_COUNT);
+    namedWindow("Video", WINDOW_NORMAL);
+    Mat frame;
+    while (cap.read(frame))
+    {
+        imshow("Video", frame);
+        ov_detector.push_img(frame);
+        cout << ov_detector.detect() << endl;
+        waitKey(1000/fps);
+    }
+
+    cap.release();
+    destroyAllWindows();
+
     return 0;
 }
